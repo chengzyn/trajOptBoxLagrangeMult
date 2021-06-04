15fac875a681c5e1cfa4cb93d9fb3e268b0d04d5
chengzyn <chengzyn@gmail.com>
2021-06-04 22:38:10 +0800

    adding function to compute mse

--- utils.py
+++ utils.py
@@ -19,3 +19,20 @@ def mat_print(mat, fmt="g"):
         for i, y in enumerate(x):
             print(("{:" + str(col_maxes[i]) + fmt + "}").format(y), end="  ")
         print("")
+
+
+def mean_squared_error(array1, array2):
+    """computes mean squared error between 2 arrays."""
+    array1 = np.array(array1)
+    array2 = np.array(array2)
+
+    difference_array = np.subtract(array1, array2)
+    squared_array = np.square(difference_array)
+    mse = squared_array.mean()
+    mse = float(mse)
+
+    return f'{mse:.3e}'
+
+
+if __name__ == '__main__':
+    print(mean_squared_error([1, 2, 3], [2, 2, 3]))
